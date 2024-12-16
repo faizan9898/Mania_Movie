613449f0cfe8c6cb5bcdb388bba447dab3083320
Faizan <faizan98arif@gmail.com>
2024-12-16 14:42:46 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/movie-mania/",
+  base: "/Mania_Movie/",
   plugins: [react()],
 })
 
